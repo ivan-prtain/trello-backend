db4ee2126a089f8429fd57e65844fb5a32102572
Ivan Prtain <ivan.prtain@borealis.biz>
2023-03-31 17:43:32 +0200

    Add create board

--- src/main/java/com/example/trellobackend/controller/BoardController.java
+++ src/main/java/com/example/trellobackend/controller/BoardController.java
@@ -4,6 +4,8 @@ import com.example.trellobackend.model.Board;
 import com.example.trellobackend.repository.BoardRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,11 @@ public class BoardController {
     @GetMapping("/{id}")
     public Board findById(@PathVariable Long id) {
         return repository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Board createBoard(@RequestBody Board board) {
+        return repository.save(board);
     }
 
 }
